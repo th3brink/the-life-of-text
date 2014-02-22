99bea3fe8cb68a0bdf11f3762e87e8ba9a8a4177
James Brinkerhoff <th3brink@gmail.com>
2014-02-21 23:21:59 -0700

    add inventory methods for the Adventure service.

--- www/js/adventure.js
+++ www/js/adventure.js
@@ -2,14 +2,31 @@
 angular.module('lifeOfText.services')
     .factory('Adventure', function($http) {
 
-        var self = {};
+        var self = {
+            silenti: {}
+        };
 
         self.getSilentiStory = function () {
             $http.get('data/theSilenti.json')
                 .then(function(res){
-                    console.log(res.data);
+                    self.silenti = res.data;
                 });
 
+        };
+        self.getSilentiStory();
+
+        self.getInventory = function () {
+            return self.silenti.players.self.inventory;
+        };
+        self.inventoryAdd = function (addMe) {
+            self.silenti.players.self.inventory.push(addMe);
+        };
+        self.inventoryRemove = function (addMe) {
+            for (var i = 0; i < self.silenti.players.self.inventory.length; i++) {
+                if (self.silenti.players.self.inventory[i]) {
+                    self.silenti.players.self.inventory.splice(i, 1);
+                }
+            }
         };
 
         return self;
