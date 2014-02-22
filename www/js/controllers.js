ec70c90c06c7cd428fabeb40da90e895c01690ad
James Brinkerhoff <th3brink@gmail.com>
2014-02-22 16:03:06 -0700

    hard coded the intro text

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,6 +7,23 @@ angular.module('lifeOfText.controllers', []).controller('TermIndexCtrl', functio
         name: "The Silenti"
     };
 
+    var adventure = {
+        name: "The Silenti",
+        type: "Horror",
+        description: "You find yourself in a dark, smoggy town called Silenti. You are here to find your friend who invited you a few weeks ago to come visit. He hasn't picked up his phone in the past few hours.",
+        "creator": "The Life of Text",
+        "storyBy": "James Brinkerhoff"
+    };
+
+    $scope.console.push({
+        input:  '>'+adventure.name,
+        output: ''
+    });
+    $scope.console.push({
+        input:  'You find yourself in a dark, smoggy town called Silenti. You are here to find your friend who invited you a few weeks ago to come visit. He hasn\'t picked up his phone in the past few hours.',
+        output: ''
+    });
+
     $scope.submit = function() {
         if ($scope.command) {
             var parsed = Parser.parseCommand($scope.command);
