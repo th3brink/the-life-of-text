468eab877ff797cde651c51948320ee2501c98aa
aharris88 <aharris88@gmail.com>
2014-02-21 17:46:02 -0700

    Change syntax structure- move into verbs object

--- www/js/parser.js
+++ www/js/parser.js
@@ -6,26 +6,25 @@ var parser = function() {
     "key",
     "torch"
   ];
-  var verbs = [
-    "inventory",
-    "inv",
-    "go",
-    "look",
-    "put",
-    "use",
-    "take",
-    "help"
-  ];
-  var directions = [
-    "north", "n",
-    "northeast", "ne",
-    "east", "e",
-    "southeast",
-    "south", "s",
-    "southwest", "sw",
-    "west", "w",
-    "northwest", "nw"
-  ];
+  var verbs = {
+    "inventory": {"variations": ["inv", "inventory"], "structure": "inventory"},
+    "go": {"variations": ["go", "walk"], "structure": "go {{direction}}"},
+    "look": {"structure": "look [[at] {{object}}]"},
+    "put": {"structure": "put {{object}} on {{object}}"},
+    "use": {"structure": "use {{object}} [on {{object}}]"},
+    "take": {"variations": ["take", "grab"], "structure": "take {{object}}"},
+    "help": {"structure": "help [{{object}}]"}
+  };
+  var directions = {
+    "north": {"variations": ["n", "north"]},
+    "northeast": {"variations": ["ne", "northeast"]},
+    "east": {"variations": ["e", "east"]},
+    "southeast": {"variations": ["se", "southeast"]},
+    "south": {"variations": ["s", "south"]},
+    "southwest": {"variations": ["sw", "southwest"]},
+    "west": {"variations": ["w", "west"]},
+    "northwest": {"variations": ["nw", "northwest"]}
+  };
   var prepositions = [
     "at",
     "on",
@@ -44,61 +43,48 @@ var parser = function() {
   var parseCommand = function(words) {
     var array = splitCommandIntoArray(words);
     parseArrayIntoSyntaxJson(array);
-    var json = fitsSyntax();
-    if(json.success) {
-      return syntax;
-    } else {
-      return json.message;
+    var json = syntaxFitsVerbsSyntax();
+  };
+
+  var syntaxFitsVerbsSyntax = function() {
+    if(syntax[0].type === "verb") {
+      fitsSyntax(getSyntaxStructureOfVerb(syntax[0].word));
+    } else if(syntax[0].type === "direction") {
+      //---------->
     }
   };
 
-  var fitsSyntax = function() {
-    var sentenceStructure = getSentenceStructureFromSyntax();
-    var result = isPattern(sentenceStructure);
-    if(result) {
-      return {"success": true};
-    } else {
-      return {"success": false, "message": getSyntaxError()};
-    }
+  var fitsSyntax = function(sentenceStructure) {
+    console.log("fitsSyntax");
+    console.log(syntax);
+    console.log(sentenceStructure);
   };
 
-  var isPattern = function(sentenceStructure) {
-    for(var prop in patterns) {
-      if(patterns[prop] === sentenceStructure) {
-        return prop;
-      }
-    }
-    return false;
-  };
-
-  var getSentenceStructureFromSyntax = function() {
-    var structure = "";
-    for(var i = 0; i < syntax.length; i++) {
-      structure += syntax[i].type + " ";
-    }
-    return structure.trim();
-  };
-
-  var getSyntaxError = function () {
+  /*var getSyntaxError = function () {
     return "I don't understand.";
-  };
+  };*/
 
   var parseArrayIntoSyntaxJson = function(wordsInArray) {
     var type;
+    var word;
     syntax = [];
     for(var i = 0; i < wordsInArray.length; i++) {
-      if(isObject(wordsInArray[i])) {
+      if(isObject(word = wordsInArray[i])) {
         type = "object";
       } else if (isVerb(wordsInArray[i])) {
+        word = isVerb(wordsInArray[i]);
         type = "verb";
       } else if (isDirection(wordsInArray[i])) {
+        word = isDirection(wordsInArray[i]);
         type = "direction";
       } else if (isPreposition(wordsInArray[i])) {
+        word = isPreposition(wordsInArray[i]);
         type = "preposition";
       } else {
+        word = wordsInArray[i];
         type = "nothing";
       }
-      syntax.push({"word": wordsInArray[i], "type": type});
+      syntax.push({"word": word, "type": type});
     }
   };
 
@@ -107,15 +93,62 @@ var parser = function() {
   };
 
   isVerb = function(word) {
-    return inArray(word, verbs);
+    return variationOfWordIsInObject(word, verbs);
   };
 
   isDirection = function(word) {
-    return inArray(word, directions);
+    return variationOfWordIsInObject(word, directions);
   };
 
   isPreposition = function(word) {
     return inArray(word, prepositions);
+  };
+
+  getSyntaxStructureOfVerb = function(word) {
+    var variations;
+    for(var prop in verbs) {
+      if(prop === word) {
+        return verbs[prop].structure;
+      } else {
+        if(typeof verbs[prop].variations !== "undefined") {
+          variations = verbs[prop].variations;
+          for(var i = 0; i < variations.length; i++) {
+            if(variations[i] === word) {
+              return verbs[prop].structure;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+
+  variationOfWordIsInObject = function(word, object) {
+    var variations;
+    for(var prop in object) {
+      if(prop === word) {
+        return prop;
+      } else {
+        if(typeof object[prop].variations !== "undefined") {
+          variations = object[prop].variations;
+          for(var i = 0; i < variations.length; i++) {
+            if(variations[i] === word) {
+              return prop;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+
+  inObject = function(word, object) {
+    for(var prop in object) {
+      if(prop === word) {
+        return true;
+      }
+    }
+    return false;
   };
 
   inArray = function(word, array) {
@@ -133,6 +166,7 @@ var parser = function() {
 
   return {
     parseCommand: parseCommand,
+    variationOfWordIsInObject: variationOfWordIsInObject,
     parseArrayIntoSyntaxJson: parseArrayIntoSyntaxJson,
     splitCommandIntoArray: splitCommandIntoArray
   };
